fd7d595c2a9c1fd947a2f2cf875581f1c3156ba7
StefanPahlplatz <stefanpahlplatz@inogo.nl>
2020-03-10 21:24:20 +0100

    Session optimization

--- src/app/application.go
+++ src/app/application.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"github.com/StefanPahlplatz/bookstore_oauth-api/src/clients/cassandra"
 	"github.com/StefanPahlplatz/bookstore_oauth-api/src/domain/access_token"
 	"github.com/StefanPahlplatz/bookstore_oauth-api/src/http"
 	"github.com/StefanPahlplatz/bookstore_oauth-api/src/repository/db"
@@ -11,12 +10,6 @@ import (
 var router = gin.Default()
 
 func StartApplication() {
-	session, err := cassandra.GetSession()
-	if err != nil {
-		panic(err)
-	}
-	session.Close()
-
 	atHandler := http.NewHandler(access_token.NewService(db.NewRepository()))
 
 	router.GET("oauth/access_token/:access_token_id", atHandler.GetById)
